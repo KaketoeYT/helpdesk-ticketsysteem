6148f26e144d890ce4c45e08bdeadf5d593c634d
KaketoeYT <Thijsderogee@gmail.com>
2026-02-04 13:27:42 +0100

    ticket model

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    public function category()
+    {
+        return $this->hasOne(Category::class);
+    }
+
+    public function chats()
+    {
+        return $this->hasMany(Chat::class);
+    }
 }
