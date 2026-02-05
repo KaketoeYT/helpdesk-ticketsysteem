3309ea66d0b70824e6930e8059a51bf08f7a6604
Hidde Manz <hiddemanz@gmail.com>
2026-02-05 12:24:07 +0100

    fixed typo

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::middleware(['auth'])->group(function () {
 Route::get('/tickets', [TicketController::class, 'index'])->name('tickets.index');
 
 Route::get('/tickets/create', [TicketController::class, 'create'])->name('tickets.create');
-Route::post('/tickets', [TicketController::class, 'store'])->name('tickets.store');
+Route::post('/tickets', [TicketController::class, 'store'])->name('ticket.store');
 
 Route::delete('/tickets/{id}', [TicketController::class, 'destroy'])->name('tickets.destroy');
 
