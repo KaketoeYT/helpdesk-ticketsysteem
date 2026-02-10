d4e467dba37893a6b01dadfd4ba4b1f0565974a2
ItsHidde <191319@novacollege.nl>
2026-02-10 13:21:37 +0100

    create en edit user aangemaakt

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,36 @@
+<x-base-layout>
+
+<div>
+    <h1>User Bewerken</h1>
+    <button><a href="{{ route('users.index') }}">Terug</a></button>
+
+    <form action="{{ route('users.update', $user->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        
+        <div>
+            <label for="name">Naam:</label>
+            <input type="text" id="name" name="name" value="{{ old('name', $user->name) }}" required>
+        </div>
+        <div>
+            <label for="email">Email:</label>
+            <input type="text" id="email" name="email" value="{{ old('email', $user->email) }}" required>
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="text" id="password" name="password" value="{{ old('password', $user->password) }}" required>
+        </div>
+        <div>
+            <label for="role">Role:</label>
+            <select id="role" name="role" required>
+                <option value="">-- kies role --</option>
+                <option value="admin" {{ old('role', $user->role) == 'admin'}}>Admin</option>
+                <option value="worker" {{ old('role', $user->role) == 'worker'}}>Worker</option>
+                <option value="user" {{ old('role', $user->role) == 'user'}}>User</option>
+            </select>
+        </div>
+        
+        <button type="submit">User Bijwerken</button>
+    </form>
+
+</x-base-layout>
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,46 @@
+<x-base-layout>
+
+<div class="container">
+    <h1>User Overzicht</h1>
+    <button><a href="{{ route('users.create') }}">Maak een nieuwe user</a></button>
+    
+    @if($users->isEmpty())
+        <p>Er zijn geen users.</p>
+    @else
+        <table border="1" cellpadding="8" cellspacing="0">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Naam</th>
+                    <th>Role</th>
+                    <th>Created at</th>
+                    <th>Acties</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($users as $user)
+                    <tr>
+                        <td>{{ $user->id }}</td>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->created_at }}</td>
+                        <td>{{ $user->role }}</td>
+                        <td>
+                            <!-- Actions buttons -->
+                            <!-- Edit -->
+                            <button><a href="{{ route('users.edit', $user->id) }}">Edit</a></button>
+
+                            <!-- Delete -->
+                            <form action="{{ route('users.destroy', $user->id) }}" method="POST" style="display:inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" onclick="return confirm('Weet je het zeker?')">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+</div>
+
+</x-base-layout>
