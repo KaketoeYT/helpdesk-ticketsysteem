e28c5e7e02a822c1acf6c2f480155aac0a83da67
ItsHidde <191319@novacollege.nl>
2026-02-11 13:51:20 +0100

    base layout fix

--- resources/views/components/base-layout.blade.php
+++ resources/views/components/base-layout.blade.php
@@ -1,11 +1,16 @@
 <nav>
-    <a href="{{route('tickets.index')}}">Tickets</a> |
-    <a href="{{route('users.index')}}">Users</a> |
-    <a href="{{route('categories.index')}}">Categories</a> |
-    <a href="{{route('statuses.index')}}">Statuses</a> |
-    <a href="{{route('locations.index')}}">Locations</a> |
-    <a href="{{route('priorities.index')}}">Priorities</a> |
-
+   @auth
+       @if (auth()->user()->role === 'admin')
+            <a href="{{route('tickets.index')}}">Tickets</a> |
+            <a href="{{route('users.index')}}">Users</a> |
+            <a href="{{route('categories.index')}}">Categories</a> |
+            <a href="{{route('statuses.index')}}">Statuses</a> |
+            <a href="{{route('locations.index')}}">Locations</a> |
+            <a href="{{route('priorities.index')}}">Priorities</a> |
+       @endif
+   @endauth        
+     
+   
     <a href="{{route('login')}}">Login</a> |
 </nav>
 
