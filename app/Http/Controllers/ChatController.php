a8a61d599214ad93abaccc0e9cd236a9ad1b8230
ItsHidde <191319@novacollege.nl>
2026-02-18 13:07:37 +0100

    chat controller made

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ChatStoreRequest;
 use App\Models\Chat;
+use App\Models\Ticket;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ChatController extends Controller
@@ -12,7 +15,9 @@ class ChatController extends Controller
      */
     public function index()
     {
-        //
+        $chats = Chat::with(['user', 'ticket'])->get();
+
+        return view('chats.index', compact('chats'));
     }
 
     /**
@@ -20,15 +25,19 @@ class ChatController extends Controller
      */
     public function create()
     {
-        //
+        $users = User::all();
+        $tickets = Ticket::all();
+        return view('chats.create', compact('users', 'tickets'));
+        
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ChatStoreRequest $request)
     {
-        //
+        $chat = Chat::create($request->validated());
+        return redirect()->route('chats.index')->with('success', 'Chat created successfully.');
     }
 
     /**
@@ -36,7 +45,7 @@ class ChatController extends Controller
      */
     public function show(Chat $chat)
     {
-        //
+        return view('chats.show', compact('chat'));
     }
 
     /**
@@ -44,15 +53,18 @@ class ChatController extends Controller
      */
     public function edit(Chat $chat)
     {
-        //
+        $users = User::all();
+        $tickets = Ticket::all();
+        return view('chats.edit', compact('chat', 'users', 'tickets'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Chat $chat)
+    public function update(ChatStoreRequest $request, Chat $chat)
     {
-        //
+        $chat->update($request->validated());
+        return redirect()->route('chats.index')->with('success', 'Chat updated successfully.');
     }
 
     /**
@@ -60,6 +72,7 @@ class ChatController extends Controller
      */
     public function destroy(Chat $chat)
     {
-        //
+        $chat->delete();
+        return redirect()->route('chats.index')->with('success', 'Chat deleted successfully.');
     }
 }
